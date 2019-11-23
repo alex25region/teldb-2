a5a26a1684180cfbf8d325088c75b45fedb518da
alex25region <Al1exXx1984>
2019-11-23 22:34:42 +1000

    Сделан ajax поиск3

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,6 @@
 // $(function() {
 //     $('#search').keyup(function () {
 //         var rex = new RegExp($(this).val(), 'i');
-//
-//
-//
 //             $('.searchable').hide();
 //             $('.searchable').filter(function () {
 //                 return rex.test($(this).text());
@@ -11,6 +8,8 @@
 //     });
 // });
 
+//-----------------------------------------------------------------------------------
+// Живой поиск по таблице
 $(document).ready(function() {
     $(function () {
         $('#search').keyup(function () {
@@ -18,14 +17,13 @@ $(document).ready(function() {
             var search = $(this).val();
 
             $('.searchable').hide();
-
             var len = $('.searchable:contains("'+search+'")').length;
-
              //console.log(len);
             if (len > 0) {
                 $('.searchable').filter(function () {
                     return rex.test($(this).text());
                 }).show().parent().prevUntil('tbody').find('.searchable').show();
+                $('.notfound').hide();
             }
             else {
                 $('.notfound').show();
@@ -33,9 +31,17 @@ $(document).ready(function() {
         });
     })
 });
-
 $.expr[":"].contains = $.expr.createPseudo(function(arg) {
     return function( elem ) {
         return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
     };
+});
+//-----------------------------------------------------------------------------------
+// Очистка строки поиска (сброс поиска)
+$("#clear").on("click", function(){
+    //code here
+    console.log('asdasdad');
+    $('#searchform')[0].reset();
+    $('.searchable').show();
+    $('.notfound').hide();
 });
--- resources/views/admin/layouts/search.blade.php
+++ resources/views/admin/layouts/search.blade.php
@@ -1,9 +1,9 @@
-<form class="form-row form-inline justify-content-end mx-2 mb-3">
+<form class="form-row form-inline justify-content-end mx-2 mb-3" id="searchform">
     <div class="row">
         <div class="input-group mr-2 w-auto shadow search">
-            <input type="text" class="form-control " placeholder="Search..." id="search" autofocus />
+            <input type="text" class="form-control" placeholder="Search..." id="search" autofocus />
             <div class="input-group-append">
-                <button class="btn btn-secondary clear" type="reset">
+                <button class="btn btn-secondary" id="clear" type="button" >
                     <i class="fa fa-times"></i>
                 </button>
             </div>
